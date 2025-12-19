911b5a80ea4148fe83d36bf79f862eafaa6503a5
Kazi Nurul Amin Sagar <sagarkazinurulamin@gmail.com>
2025-12-19 21:25:40 +0600

    Changed Social logo in the Footer.tsx
    Removed Twitter logo and added X logo in the Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Instagram, Twitter, Facebook, Youtube } from 'lucide-react';
+import { Instagram, Facebook, Youtube } from 'lucide-react';
 
 export const Footer: React.FC = () => {
   return (
@@ -15,7 +15,17 @@ export const Footer: React.FC = () => {
 
           <div className="flex gap-6">
             <a href="#" className="hover:text-brand-lime transition-colors"><Instagram size={24} /></a>
-            <a href="#" className="hover:text-brand-lime transition-colors"><Twitter size={24} /></a>
+            <a href="#" className="hover:text-brand-lime transition-colors" aria-label="X (formerly Twitter)">
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                fill="currentColor" 
+                viewBox="0 0 24 24"
+              >
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+              </svg>
+            </a>
             <a href="#" className="hover:text-brand-lime transition-colors"><Facebook size={24} /></a>
             <a href="#" className="hover:text-brand-lime transition-colors"><Youtube size={24} /></a>
           </div>
